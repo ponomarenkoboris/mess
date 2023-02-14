051f1e501d355c29893bdabf733295f5e4ad9b40
Boris Ponomarenko <borisponomarenko.dmi@gmail.com>
2023-02-14 16:53:07 +0300

    Append logic to settings page

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,65 +1,121 @@
-import { FC, useReducer, useEffect, useState } from 'react';
+import { FC, useState, useReducer, useEffect } from 'react';
 import { useAppSelector } from '@hooks/storeHooks/storeHooks';
 import { objectsAreEqual } from '@utils/utils';
-import { reducer, ActionType } from './settings.utils';
-import lady from '@assets/users/lady.png';
+import { reducer, ActionType, updateProfilePhoto, callClickEvent, tooglePasswordVisibility } from './settings.utils';
+import closeEye from '@assets/registration/close_eye.svg'
 import './Settings.scss';
 
-export const Settings: FC = () => {
-    const { username, email, name } = useAppSelector((store) => store.user);
-    const initialState = { username, email, name };
-    const [user, dispatch] = useReducer(reducer, initialState);
-    const [isSubmitAvaliable, setIsSubmitAvaliable] = useState<boolean>(false);
 
-    const applayChanges = () => {
+export const Settings: FC = () => {
+    const { username, email, name, imageSrc } = useAppSelector((store) => store.user);
+    const initialState = { username, email, name, imageSrc };
+    const [pass, setPass] = useState({ password: '', rePassword: '' })
+    const [user, dispatch] = useReducer(reducer, initialState);
+    const [isProfileSubmitAvaliable, setIsProfileSubmitAvaliable] = useState<boolean>(false);
+
+    const applayProfileChanges = () => {
         // TODO applay user profile changes
     };
 
-    useEffect(() => {
-        if (!objectsAreEqual(initialState, user)) setIsSubmitAvaliable(true);
-        else setIsSubmitAvaliable(false);
-    }, [user]);
+    const applayPasswordChanges = () => {
+        // TODO applay user password changes
+    };
 
+    useEffect(() => {
+        if (!objectsAreEqual(initialState, user)) setIsProfileSubmitAvaliable(true);
+        else setIsProfileSubmitAvaliable(false);
+    }, [user]);
+ 
     return (
         <main className='settings__page'>
             <article className='public__info'>
                 <h1>Profile</h1>
                 <div className='user-main-info'>
-                    <img src={lady} alt='Profile' />
+                    <div className="photo__uploader">
+                        <input type="file" accept='.jpg, .jpeg, .png' onChange={updateProfilePhoto(dispatch)} />
+                        <button className="photo__uploader" onClick={callClickEvent}>
+                            <img src={user.imageSrc} alt='Profile' />
+                        </button>
+                    </div>
                     <div className='info'>
-                        <label className='name-info'>
-                            <input
-                                type='text'
-                                onChange={(e) => dispatch({ type: ActionType.UPDATE_NAME, payload: e.target.value })}
-                                defaultValue={user.name}
-                            />
-                            <div className='writeble-element-indicator'></div>
-                        </label>
-                        <label className='email-info'>
-                            <input
-                                type='text'
-                                onChange={(e) => dispatch({ type: ActionType.UPDATE_EMAIL, payload: e.target.value })}
-                                defaultValue={user.email}
-                            />
-                            <div className='writeble-element-indicator'></div>
-                        </label>
-                        <label className='username-info'>
-                            <input
-                                type='text'
-                                onChange={(e) => dispatch({ type: ActionType.UPDATE_USERNAME, payload: e.target.value })}
-                                defaultValue={user.username}
-                            />
-                            <div className='writeble-element-indicator'></div>
-                        </label>
+                        <div className="info__name">
+                            <p>Name: </p>
+                            <label className='name'>
+                                <input
+                                    type='text'
+                                    onChange={(e) => dispatch({ type: ActionType.UPDATE_NAME, payload: e.target.value })}
+                                    defaultValue={user.name}
+                                />
+                                <div className='writeble-element-indicator'></div>
+                            </label>
+                        </div>
+                        <div className="info__email">
+                            <p>Email: </p>
+                            <label className='email'>
+                                <input
+                                    type='text'
+                                    onChange={(e) => dispatch({ type: ActionType.UPDATE_EMAIL, payload: e.target.value })}
+                                    defaultValue={user.email}
+                                />
+                                <div className='writeble-element-indicator'></div>
+                            </label>
+                        </div>
+                        <div className="info__username">
+                            <p>Username: </p>
+                            <label className='username'>
+                                <input
+                                    type='text'
+                                    onChange={(e) => dispatch({ type: ActionType.UPDATE_USERNAME, payload: e.target.value })}
+                                    defaultValue={user.username}
+                                />
+                                <div className='writeble-element-indicator'></div>
+                            </label>
+                        </div>
                     </div>
                 </div>
-                <div className='submit-wrapper'>
-                    <button className='submit-changes' style={{ visibility: isSubmitAvaliable ? 'visible' : 'hidden' }} onClick={applayChanges}>
-                        Submit
+                <div className='submit-profile'>
+                    <button 
+                        className='submit-profile-changes' 
+                        style={{ visibility: isProfileSubmitAvaliable ? 'visible' : 'hidden' }}
+                        onClick={applayProfileChanges}
+                    >
+                        Confirm profile changes
                     </button>
                 </div>
             </article>
-            <article className='password__info'></article>
+            <article className='password__info'>
+                <h1>Password</h1>
+                <div className="password__form">
+                    <div className="password">
+                        <input 
+                            type="password" 
+                            placeholder='Input new password' 
+                            onChange={(e) => setPass(prev => ({ ...prev, password: e.target.value }))} 
+                        />
+                        <button className="visibility-contorller" onClick={tooglePasswordVisibility}>
+                            <img src={closeEye} alt="See password" />
+                        </button>
+                    </div>
+                    <div className="rePassword">
+                        <input 
+                            type="password" 
+                            placeholder='Confirm password' 
+                            onChange={(e) => setPass(perv => ({ ...perv, rePassword: e.target.value }))} 
+                        />
+                        <button className="visibility-contorller" onClick={tooglePasswordVisibility}>
+                            <img src={closeEye} alt="See password" />
+                        </button>
+                    </div>
+                </div>
+                <div className="submit-password">
+                    <button 
+                        className='submit-password-changes' 
+                        style={{ visibility: pass.password && pass.rePassword && pass.password === pass.rePassword ? 'visible' : 'hidden' }} 
+                        onClick={applayPasswordChanges}>
+                            Confirm password
+                    </button>
+                </div>
+            </article>
         </main>
     );
 };
--- src/pages/settings/settings.utils.ts
+++ src/pages/settings/settings.utils.ts
@@ -1,11 +1,15 @@
 import { IUser } from '@store/models/user.model';
+import { ChangeEvent, Dispatch, MouseEvent } from 'react';
+import openEye from '@assets/registration/open_eye.svg'
+import closeEye from '@assets/registration/close_eye.svg'
 
-type User = Pick<IUser, 'username' | 'email' | 'name'>;
+type User = Pick<IUser, 'username' | 'email' | 'name' | 'imageSrc'>;
 
 enum ActionType {
     UPDATE_USERNAME = 'UPDATE_USERNAME',
     UPDATE_EMAIL = 'UPDATE_EMAIL',
     UPDATE_NAME = 'UPDATE_NAME',
+    UPDATE_IMAGE = 'UPDATE_IMAGE'
 }
 
 type Action = {
@@ -21,9 +25,43 @@ const reducer = (state: User, action: Action): User => {
             return { ...state, email: action.payload };
         case ActionType.UPDATE_NAME:
             return { ...state, name: action.payload };
+        case ActionType.UPDATE_IMAGE:
+            return { ...state, imageSrc: action.payload };
         default:
             return state;
     }
 };
 
-export { reducer, ActionType };
+const updateProfilePhoto = (dispatch: Dispatch<Action>) => {
+
+    return (event: ChangeEvent<HTMLInputElement>) => {
+        if (!event.target.files || !event.target.files.item(0)) return
+        const file = event.target.files.item(0)
+        if (!file) return
+
+        const reader = new FileReader()
+
+        reader.addEventListener('load', () => {
+            const result = reader.result as string;
+            dispatch({ type: ActionType.UPDATE_IMAGE, payload: result })
+        })
+
+        reader.readAsDataURL(file)
+    }
+}
+
+const callClickEvent = (event: MouseEvent<HTMLButtonElement>) => {
+    const parent = event.currentTarget.parentNode as ParentNode 
+    const fileElem = parent.querySelector('[type="file"]') as HTMLInputElement
+    fileElem.click()
+}
+
+const tooglePasswordVisibility = (event: MouseEvent<HTMLButtonElement>) => {
+    const passwordInput = event.currentTarget?.parentNode?.querySelector('input');
+    const image = event.currentTarget.querySelector('img');
+    if (!passwordInput || !image) return
+    passwordInput.type = passwordInput.type === 'text' ? 'password' : 'text'
+    image.src = passwordInput.type === 'text' ? openEye : closeEye
+}
+
+export { reducer, ActionType, updateProfilePhoto, callClickEvent, tooglePasswordVisibility };
